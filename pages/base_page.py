5a13225b47d0819e7df0131a34699692f17405e2
Vorzheva Anastasiia <vorzhevaas@gmail.com>
2024-03-26 17:48:49 +0500

    Добавили тайм-аут

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,12 @@ import time
 import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+def __init__(self, browser, url, timeout=10):
+    self.browser = browser
+    self.url = url
+    self.browser.implicitly_wait(timeout)
 
 
 class BasePage():
