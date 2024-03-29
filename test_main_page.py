655d99d7fa3f6ebdabc3fe9a19066912b281f676
Vorzheva Anastasiia <vorzhevaas@gmail.com>
2024-03-29 16:16:31 +0500

    Изменена ссылка

--- test_main_page.py
+++ test_main_page.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
