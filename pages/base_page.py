1ff5f621517a852c0b3f0e8efe6637f70d241714
Vorzheva Anastasiia <vorzhevaas@gmail.com>
2024-03-29 18:26:45 +0500

    Добавили решение задачки для получения скидки

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,17 +4,38 @@ import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
-def __init__(self, browser, url, timeout=10):
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def __init__(self, browser, url, timeout=15):
     self.browser = browser
     self.url = url
     self.browser.implicitly_wait(timeout)
 
 
 class BasePage():
+    def solve_quiz_and_get_code(self):
+        time.sleep(2)
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
+
+
